c4f80bd5bb1d38cf2160a76d3570a58c3e3ded68
Alan TL <teamlemons.cr@gmail.com>
2024-05-03 17:57:41 -0300

    se trabajo en el envio y recepcion de paquetes udp para la comunicacion del A-nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,31 @@
 #include <SPI.h>
 #include <Ethernet2.h>
+#include <EthernetUdp2.h> // Incluye la biblioteca UDP
 
-// Definir la dirección IP del servidor (puede ser la misma que la del Arduino)
+
+// Definir la dirección IP del servidor
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192, 168, 0, 101);
-EthernetServer server(80);
+unsigned int serverPort = 8888; // Puerto UDP
+
+EthernetUDP udp;
+
 void setup() {
   Serial.begin(9600);
+  Ethernet.begin(mac, ip);
+  udp.begin(8888); // Inicia el UDP en el puerto 8888
 }
 
 void loop() {
-
   analogReference(DEFAULT);
-  // Leer el valor del ADC
   int adcValue = analogRead(A0);
-  
-  // Convertir el valor del ADC a voltaje (0-5V)
   float voltage = adcValue * (5.0 / 1023.0);
-  
-  // Imprimir el voltaje por el puerto serial
-  Serial.print("Voltaje: ");
-  Serial.print(voltage, 2); // Imprimir el voltaje con 2 decimales
-  Serial.println(" V");
-  
-  // Esperar un tiempo antes de la próxima lectura
-  delay(1000); // Esperar 1 segundo
+
+  /// Enviar el valor del voltaje como un solo número a través de UDP
+  udp.beginPacket(ip, serverPort);
+  udp.print(voltage); // Envía el valor del voltaje
+  udp.endPacket();
+
+  // Esperar 2 segundos antes de enviar los datos nuevamente
+  delay(2000);
 }
