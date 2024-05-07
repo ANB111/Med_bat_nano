38b5b0627f1a3890ce10bfcadba6bd5ead397b69
Alan TL <teamlemons.cr@gmail.com>
2024-05-07 11:51:58 -0300

    envio correcto de los bits del adc via UDP

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,27 @@
 #include <SPI.h>
-#include <Ethernet2.h>
-#include <EthernetUdp2.h> // Incluye la biblioteca UDP
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Dirección MAC del Arduino
+IPAddress ip(192,168,0,101); // Dirección IP del Arduino
+IPAddress serverIP(192,168,0,100); // Dirección IP del servidor
+unsigned int serverPort = 8888; // Puerto UDP del servidor
 
-// Definir la dirección IP del servidor
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192, 168, 0, 101);
-unsigned int serverPort = 8888; // Puerto UDP
-
-EthernetUDP udp;
+EthernetUDP Udp;
 
 void setup() {
   Serial.begin(9600);
   Ethernet.begin(mac, ip);
-  udp.begin(8888); // Inicia el UDP en el puerto 8888
+  Udp.begin(serverPort);
 }
 
 void loop() {
-  analogReference(DEFAULT);
-  int adcValue = analogRead(A0);
-  float voltage = adcValue * (5.0 / 1023.0);
+  int adcValue = analogRead(A0); // Lee el valor del ADC
 
-  /// Enviar el valor del voltaje como un solo número a través de UDP
-  udp.beginPacket(ip, serverPort);
-  udp.print(voltage); // Envía el valor del voltaje
-  udp.endPacket();
+  // Envía los 10 bits del valor del ADC como datos binarios a través de UDP
+  Udp.beginPacket(serverIP, serverPort);
+  Udp.write((uint8_t*)&adcValue, sizeof(adcValue));
+  Udp.endPacket();
 
-  // Esperar 2 segundos antes de enviar los datos nuevamente
-  delay(2000);
+  delay(1000); // Espera 1 segundo antes de enviar el siguiente valor
 }
