1d7596b463852abb79f333665c528939ff0700cb
Alan-B <alan@gmail.com>
2024-06-25 11:35:46 -0300

    rama para debug

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,18 @@ EthernetUDP Udp;
 
 void setup() {
   Serial.begin(9600);
-  Ethernet.begin(mac, ip);
+  Serial.println("Iniciando Ethernet...");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Fallo al configurar Ethernet con DHCP");
+    // Prueba con una IP estática si DHCP falla
+    Ethernet.begin(mac, ip);
+  }
+  Serial.println("Ethernet configurado");
+  Serial.print("Dirección IP: ");
+  Serial.println(Ethernet.localIP());
+  
   Udp.begin(serverPort);
+  Serial.println("UDP iniciado en puerto: " + String(serverPort));
 }
 
 void loop() {
@@ -22,18 +32,31 @@ void loop() {
   const int shiftAmount = 3; // Corrimiento de bits para dividir por numSamples
   int acumulador = 0;
 
+  Serial.println("Tomando muestras del ADC...");
+  
   // Tomar 8 muestras del ADC y acumularlas
   for (int i = 0; i < numSamples; i++) {
-    acumulador += analogRead(A1);
+    int muestra = analogRead(A1);
+    acumulador += muestra;
+    Serial.print("Muestra ");
+    Serial.print(i + 1);
+    Serial.print(": ");
+    Serial.println(muestra);
   }
 
   // Calcular el promedio utilizando corrimiento de bits
   int promedio = acumulador >> shiftAmount;
+  Serial.print("Promedio calculado: ");
+  Serial.println(promedio);
 
   // Enviar el promedio como un número entero a través de UDP
   Udp.beginPacket(serverIP, serverPort);
   Udp.write((uint8_t*)&promedio, sizeof(promedio));
-  Udp.endPacket();
+  if (Udp.endPacket() == 1) {
+    Serial.println("Paquete UDP enviado exitosamente.");
+  } else {
+    Serial.println("Error al enviar el paquete UDP.");
+  }
 
-  delay(1000); // Esperar
+  delay(3000); // Esperar
 }
