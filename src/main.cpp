85932bb2697b26dc2b5221df882f045db2da4407
Alan-B <alan@gmail.com>
2024-06-28 19:01:50 -0300

    codigo final, con activacion por udp

--- src/main.cpp
+++ src/main.cpp
@@ -3,37 +3,65 @@
 #include <EthernetUdp.h>
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // Dirección MAC del Arduino
-IPAddress ip(192,168,0,101); // Dirección IP del Arduino
-IPAddress serverIP(192,168,0,100); // Dirección IP del servidor
-unsigned int serverPort = 8888; // Puerto UDP del servidor
+unsigned int localPort = 8888; // Puerto UDP local para recibir datos
 
 EthernetUDP Udp;
 
+IPAddress remoteIp;
+unsigned int remotePort;
+
+bool acquisitionStarted = false;
+const char startKeyword[] = "START";
+
 void setup() {
-  Serial.begin(9600);
-  Ethernet.begin(mac, ip);
-  Udp.begin(serverPort);
+  if (Ethernet.begin(mac) == 0) {
+    while (true); // Detener si DHCP falla
+  }
+  Udp.begin(localPort);
 }
 
 void loop() {
-  analogReference(DEFAULT);
+  // Buffer para almacenar los datos entrantes
+  char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+  
+  // Comprobar si hay datos UDP disponibles
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    // Leer el paquete UDP entrante
+    int len = Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    packetBuffer[len] = 0; // Terminar el string con un null
+    
+    // Obtener la IP y el puerto del remitente
+    remoteIp = Udp.remoteIP();
+    remotePort = Udp.remotePort();
 
-  const int numSamples = 8; // Número de muestras a tomar, potencia de 2
-  const int shiftAmount = 3; // Corrimiento de bits para dividir por numSamples
-  int acumulador = 0;
-
-  // Tomar 8 muestras del ADC y acumularlas
-  for (int i = 0; i < numSamples; i++) {
-    acumulador += analogRead(A1);
+    // Comprobar el contenido del paquete
+    if (strcmp(packetBuffer, startKeyword) == 0) {
+      acquisitionStarted = true;
+    }
   }
 
-  // Calcular el promedio utilizando corrimiento de bits
-  int promedio = acumulador >> shiftAmount;
+  if (acquisitionStarted) {
+    analogReference(DEFAULT);
 
-  // Enviar el promedio como un número entero a través de UDP
-  Udp.beginPacket(serverIP, serverPort);
-  Udp.write((uint8_t*)&promedio, sizeof(promedio));
-  Udp.endPacket();
+    const int numSamples = 8; // Número de muestras a tomar, potencia de 2
+    const int shiftAmount = 3; // Corrimiento de bits para dividir por numSamples
+    int acumulador = 0;
 
-  delay(1000); // Esperar
+    // Tomar 8 muestras del ADC y acumularlas
+    for (int i = 0; i < numSamples; i++) {
+      int muestra = analogRead(A1);
+      acumulador += muestra;
+    }
+
+    // Calcular el promedio utilizando corrimiento de bits
+    int promedio = acumulador >> shiftAmount;
+
+    // Enviar el promedio como un número entero a través de UDP
+    Udp.beginPacket(remoteIp, remotePort);
+    Udp.write((uint8_t*)&promedio, sizeof(promedio));
+    Udp.endPacket();
+
+    delay(3000); // Esperar antes de tomar la siguiente lectura
+  }
 }
